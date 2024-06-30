2de12d7140d93ca3240aeb13f4ba1aa41e636956
M Lau <63963445+tofuuudon@users.noreply.github.com>
2024-06-30 18:46:46 +0100

    feat(git): add module and commit fn (#3)

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ use clap::Parser;
 
 use format::format::cc_message;
 use format::format::cc_type;
+use git::git::commit;
 
 mod format;
+mod git;
 
 #[derive(Parser)]
 struct Args {
@@ -15,6 +17,7 @@ fn main() {
 
     let cc_type = cc_type(args.r#type.as_str());
     let cc_message = cc_message(cc_type);
+    let cc_commit = format!("{}: {}", cc_type, cc_message);
 
-    println!("{}: {}", cc_type, cc_message);
+    commit(&cc_commit);
 }
