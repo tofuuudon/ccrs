f9945debbfd406fb30576e1eee38ca6e102a7591
Mikey Lau <63963445+tofuuudon@users.noreply.github.com>
2024-07-14 22:29:38 +0100

    fix: add homebrew formula

---
+++ ccrs.rb
@@ -0,0 +1,14 @@
+class Ccrs < Formula
+  desc "A CLI tool for creating conventional commits"
+  homepage "https://github.com/tofuuudon/ccrs"
+  url "https://github.com/tofuuudon/ccrs/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "b1f7f047e8ec41eb5cab2cc152734ffcc7481ada96ddcbb17f8219e4b494bd48"
+  
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "build", "--release", "--bin", "ccrs"
+    bin.install "target/release/ccrs"
+  end
+end
+
