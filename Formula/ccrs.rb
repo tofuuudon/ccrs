557123c2aadd57b7af0f81ccacd59a11ae2721b2
Mikey Lau <63963445+tofuuudon@users.noreply.github.com>
2024-07-14 22:57:26 +0100

    fix: update formula url

--- Formula/ccrs.rb
+++ Formula/ccrs.rb
@@ -1,7 +1,7 @@
 class Ccrs < Formula
   desc "A CLI tool for creating conventional commits"
   homepage "https://github.com/tofuuudon/ccrs"
-  url "https://github.com/tofuuudon/ccrs/archive/refs/tags/v0.1.0.tar.gz"
+  url "https://github.com/tofuuudon/ccrs/releases/download/v0.1.0/ccrs.tar.gz"
   sha256 "276f6f8aa3f60a1521a81990024de806d65e650145fc15311bf1167d53971257"
 
   def install
