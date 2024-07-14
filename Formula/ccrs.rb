5ae4c6d91ba1d4e6150631fc2b2672bfb842556e
Mikey Lau <63963445+tofuuudon@users.noreply.github.com>
2024-07-14 22:46:45 +0100

    fix: update checksum

--- Formula/ccrs.rb
+++ Formula/ccrs.rb
@@ -2,7 +2,7 @@ class Ccrs < Formula
   desc "A CLI tool for creating conventional commits"
   homepage "https://github.com/tofuuudon/ccrs"
   url "https://github.com/tofuuudon/ccrs/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "b1f7f047e8ec41eb5cab2cc152734ffcc7481ada96ddcbb17f8219e4b494bd48"
+  sha256 "037c9f91b074a01faadc719dc3ba341426fc9a973deaf906cfd7435b2252150f"
   
   depends_on "rust" => :build
 
