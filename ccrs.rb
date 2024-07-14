c8d9151f2491667ac781b88424c29c075fd3a6bc
Mikey Lau <63963445+tofuuudon@users.noreply.github.com>
2024-07-14 22:36:34 +0100

    fix: repository name

--- ccrs.rb
+++ ccrs.rb
@@ -1,7 +1,7 @@
 class Ccrs < Formula
   desc "A CLI tool for creating conventional commits"
-  homepage "https://github.com/tofuuudon/ccrs"
-  url "https://github.com/tofuuudon/ccrs/archive/refs/tags/v0.1.0.tar.gz"
+  homepage "https://github.com/tofuuudon/homebrew-ccrs"
+  url "https://github.com/tofuuudon/homebrew-ccrs/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "b1f7f047e8ec41eb5cab2cc152734ffcc7481ada96ddcbb17f8219e4b494bd48"
   
   depends_on "rust" => :build
