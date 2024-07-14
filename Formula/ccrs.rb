7bbacce8f6f6c819d30269a20149267348609fac
Mikey Lau <63963445+tofuuudon@users.noreply.github.com>
2024-07-14 22:55:54 +0100

    fix: update formula

--- Formula/ccrs.rb
+++ Formula/ccrs.rb
@@ -2,13 +2,10 @@ class Ccrs < Formula
   desc "A CLI tool for creating conventional commits"
   homepage "https://github.com/tofuuudon/ccrs"
   url "https://github.com/tofuuudon/ccrs/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "037c9f91b074a01faadc719dc3ba341426fc9a973deaf906cfd7435b2252150f"
-  
-  depends_on "rust" => :build
+  sha256 "276f6f8aa3f60a1521a81990024de806d65e650145fc15311bf1167d53971257"
 
   def install
-    system "cargo", "build", "--release", "--bin", "ccrs"
-    bin.install "target/release/ccrs"
+    bin.install "ccrs"
   end
 end
 
