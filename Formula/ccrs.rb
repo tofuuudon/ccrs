b8ba5225fa05cfd3451e889c80a17ce11f1d40b8
Mikey Lau <63963445+tofuuudon@users.noreply.github.com>
2024-07-19 22:47:42 +0100

    chore: bump cargo version and update checksum

--- Formula/ccrs.rb
+++ Formula/ccrs.rb
@@ -1,8 +1,8 @@
 class Ccrs < Formula
   desc "A CLI tool for creating conventional commits"
   homepage "https://github.com/tofuuudon/ccrs"
-  url "https://github.com/tofuuudon/ccrs/releases/download/v0.1.0/ccrs.tar.gz"
-  sha256 "276f6f8aa3f60a1521a81990024de806d65e650145fc15311bf1167d53971257"
+  url "https://github.com/tofuuudon/ccrs/releases/download/v0.2.0/ccrs.tar.gz"
+  sha256 "de90655a7028fe9ff0a7816119a32555d5a67418e118370d18db9aeb1fc39b5f"
 
   def install
     bin.install "ccrs"
