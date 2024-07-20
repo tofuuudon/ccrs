06aae3400a47c81f791755d6b894004ca68f78b4
Mikey Lau <63963445+tofuuudon@users.noreply.github.com>
2024-07-20 11:50:41 +0100

    chore: update version on cargo and formula

--- Formula/ccrs.rb
+++ Formula/ccrs.rb
@@ -1,7 +1,7 @@
 class Ccrs < Formula
   desc "A CLI tool for creating conventional commits"
   homepage "https://github.com/tofuuudon/ccrs"
-  url "https://github.com/tofuuudon/ccrs/releases/download/v0.2.0/ccrs.tar.gz"
+  url "https://github.com/tofuuudon/ccrs/releases/download/v0.2.1/ccrs.tar.gz"
   sha256 "da93bd8c49e80bf760f125f3c96c2aefd7be68e77dfc382ca09811b4daf02d3e"
 
   def install
