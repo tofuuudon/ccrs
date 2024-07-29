dc8a8aa9a29f81aaac8e471378c01ff22b36b2f3
Mikey Lau <63963445+tofuuudon@users.noreply.github.com>
2024-07-29 12:29:05 +0100

    chore: update checksum on formula for v0.2.2

--- Formula/ccrs.rb
+++ Formula/ccrs.rb
@@ -1,8 +1,8 @@
 class Ccrs < Formula
   desc "A CLI tool for creating conventional commits"
   homepage "https://github.com/tofuuudon/ccrs"
-  url "https://github.com/tofuuudon/ccrs/releases/download/v0.2.1/ccrs.tar.gz"
-  sha256 "9df7a61d395cfd676320a346c3f95c50be533f470fac9b00bb90fb41b1082490"
+  url "https://github.com/tofuuudon/ccrs/releases/download/v0.2.2/ccrs.tar.gz"
+  sha256 "aff008587f1f0760580182d99a4df970dfc4e4ce81d45dccdc7fff75301e5e30"
 
   def install
     bin.install "ccrs"
